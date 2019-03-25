5004078449aff070903af58423535c28226e5d3a
AndrewIvlev <mr.andrew.ivlev@gmail.com>
2019-03-26 01:41:54 +0300

    init commit

--- SYNCHRONIZATOR/Form1.Designer.cs
+++ SYNCHRONIZATOR/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.w1TextBox = new System.Windows.Forms.TextBox();
+            this.trySync = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotView1.Location = new System.Drawing.Point(12, 12);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(560, 313);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ω₁=";
+            // 
+            // w1TextBox
+            // 
+            this.w1TextBox.Location = new System.Drawing.Point(57, 336);
+            this.w1TextBox.Name = "w1TextBox";
+            this.w1TextBox.Size = new System.Drawing.Size(39, 20);
+            this.w1TextBox.TabIndex = 2;
+            // 
+            // trySync
+            // 
+            this.trySync.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.trySync.Location = new System.Drawing.Point(439, 328);
+            this.trySync.Name = "trySync";
+            this.trySync.Size = new System.Drawing.Size(133, 31);
+            this.trySync.TabIndex = 3;
+            this.trySync.Text = "Try To Sync";
+            this.trySync.UseVisualStyleBackColor = true;
+            this.trySync.Click += new System.EventHandler(this.trySync_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(117, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "d=";
+            // 
+            // dTextBox
+            // 
+            this.dTextBox.Location = new System.Drawing.Point(151, 336);
+            this.dTextBox.Name = "dTextBox";
+            this.dTextBox.Size = new System.Drawing.Size(39, 20);
+            this.dTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.dTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trySync);
+            this.Controls.Add(this.w1TextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plotView1);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.Text = "SYNCHRONIZATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox w1TextBox;
+        private System.Windows.Forms.Button trySync;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dTextBox;
     }
 }
 
--- SYNCHRONIZATOR/Form1.cs
+++ SYNCHRONIZATOR/Form1.cs
@@ -1,20 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Extreme.Mathematics;
+using Extreme.Mathematics.Calculus.OrdinaryDifferentialEquations;
+
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace SYNCHRONIZATOR
 {
     public partial class Form1 : Form
     {
+        RungeKuttaFehlbergIntegrator rkf45;
+        public static double w1, w2, d, delta;
+
         public Form1()
         {
             InitializeComponent();
+
+            rkf45 = new RungeKuttaFehlbergIntegrator();
+
+            var myModel = new PlotModel { Title = "synchronization" };
+            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 150, 0.1, "cos(x)"));
+            this.plotView1.Model = myModel;
+        }
+
+        private void trySync_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(w1TextBox.Text, out w1)) return;
+            if (double.TryParse(dTextBox.Text, out d)) return;
+            delta = 
+            rkf45.InitialTime = 0.0;
+            rkf45.InitialValue = Vector.Create(1.0, 0.0);
+            rkf45.DifferentialFunction = TwoDifEqSystem;
+            rkf45.AbsoluteTolerance = 1e-8;
+
+            Console.WriteLine("Classic 4/5th order Runge-Kutta-Fehlberg");
+            for (int i = 0; i <= 100; i++)
+            {
+                double t = i;
+                var y = rkf45.Integrate(t);
+                Console.WriteLine("{0:F2}: {1,20:F14} ({2} steps)", t, y, rkf45.IterationsNeeded);
+            }
+        }
+
+        private static Vector<double> TwoDifEqSystem(double t, Vector<double> fi, Vector<double> dfi)
+        {
+            if (dfi == null)
+                dfi = Vector.Create<double>(3);
+
+            double sigma = 10.0;
+            double beta = 8.0 / 3.0;
+            double rho = 28.0;
+
+            dfi[0] = w1 + d * Math.Sin(fi[1] - fi[0]);
+            dfi[1] = w2 + d * Math.Sin(fi[0] - fi[1]);
+
+            return dfi;
         }
     }
 }
